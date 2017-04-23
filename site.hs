74b63efbc63196a437990af3e747d87058304014
Michal Kawalec <michal@monad.cat>
2017-04-23 17:44:33 +0200

    site looks decent

--- site.hs
+++ site.hs
@@ -1,67 +1,86 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
+import qualified Debug.Trace as DT
 import           Hakyll
+import           Data.Monoid ((<>))
 
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+  match "images/*" $ do
+    route   idRoute
+    compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+  match "css/*" $ do
+    route   idRoute
+    compile compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $ pandocCompiler
+      >>= saveSnapshot "rawContent"
+      >>= loadAndApplyTemplate "templates/post.html" blogCtx
+      >>= saveSnapshot "content"
+      >>= loadAndApplyTemplate "templates/default.html" blogCtx
+      >>= relativizeUrls
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+  match "pages/*" $ do
+    route $ setExtension "html"
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let teaserCtx = teaserField "teaser" "rawContent" `mappend` blogCtx
+      let pagesCtx =
+            listField "posts" teaserCtx (return posts) `mappend`
+            blogCtx
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+      getResourceBody
+        >>= applyAsTemplate pagesCtx
+        >>= loadAndApplyTemplate "templates/default.html" blogCtx
+        >>= relativizeUrls
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      let indexCtx =
+            constField "title" "Home"                `mappend`
+            blogCtx
 
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" blogCtx
+        >>= relativizeUrls
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
+  match "templates/*" $ compile templateBodyCompiler
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
-
-    match "templates/*" $ compile templateBodyCompiler
+  create ["atom.xml"] $ do
+    route idRoute
+    compile $ do
+        let feedCtx = blogCtx `mappend` bodyField "description"
+        posts <- fmap (take 10) . recentFirst =<<
+          loadAllSnapshots "posts/*" "content"
+        renderAtom myFeedConfiguration feedCtx posts
 
 
 --------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+
+isPage :: String -> Item String -> Bool
+isPage pageName item = pageName == (toFilePath $ itemIdentifier item)
+
+blogCtx :: Context String
+blogCtx =
+  boolField "isRoot" (isPage "index.html") `mappend`
+  boolField "isPosts" (isPage "pages/posts.html") `mappend`
+  constField "currentYear" "2017" `mappend`
+  dateField "date" "%B %e, %Y" `mappend`
+  defaultContext
+
+myFeedConfiguration :: FeedConfiguration
+myFeedConfiguration = FeedConfiguration
+    { feedTitle       = "MonadCat: Latest posts"
+    , feedDescription = "Words about Haskell, Maths, JS and life"
+    , feedAuthorName  = "Michal Kawalec"
+    , feedAuthorEmail = "michal@monad.cat"
+    , feedRoot        = "https://monad.cat"
+    }
